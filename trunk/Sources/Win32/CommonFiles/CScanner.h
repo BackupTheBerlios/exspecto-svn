e79ce34cee8a4b74477b57a07527e5999fbd6f54
parshind <parshind@284ff1dd-de15-0410-891b-f61abf421a23>
2006-10-03 20:11:43 +0000

    Доработан класс Container Разработана стратегия загрузки плагинов (PluginLoadStrategy)

---
+++ trunk/Sources/Win32/CommonFiles/CScanner.h
@@ -0,0 +1,18 @@
+#ifndef CSCANNER_H_
+#define CSCANNER_H_
+
+#include <string>
+#include <vector>
+
+class CScanner
+{
+public:
+	CScanner(){};
+	virtual ~CScanner(){};
+		
+	virtual void Scan( IN std::string strAddress, OUT std::vector< std::string >& vecResList ) = 0;
+	
+	virtual const char* GetProtocolName();
+
+};
+#endif //CSCANNER_H_
