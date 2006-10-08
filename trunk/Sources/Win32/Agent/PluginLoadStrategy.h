e90149ffd484f5285e14bbe2d422d81775fc3b3a
parshind <parshind@284ff1dd-de15-0410-891b-f61abf421a23>
2006-10-08 19:54:13 +0000

    Изменения в ScanServer(модель,реализация)

---
+++ trunk/Sources/Win32/Agent/PluginLoadStrategy.h
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------------------//
+//Этот файл является частью проекта Exspecto 2006г.
+//Module: Plugin load strategy
+//Author: Parshin Dmitry
+//Description: Класс, описывающий стратегию загрузки плагинов в класс Container
+//-------------------------------------------------------------------------------------//
+#ifndef PLUGINLOADSTRATEGY_H_
+#define PLUGINLOADSTRATEGY_H_
+
+#include "windows.h"
+#include "CScanner.h"
+#include <map>
+
+class PluginLoadStrategy
+{
+public:
+	PluginLoadStrategy( std::vector< CScanner* >& vecStorage );
+	virtual ~PluginLoadStrategy();
+	
+private:	
+	//Типы функций, содержащихся в dll с plugin-ом
+	typedef CScanner* (*fnGetScannerFunc)();
+	typedef void (*fnReleaseScannerFunc)();
+	
+
+	typedef std::map< HINSTANCE , fnReleaseScannerFunc > LibrariesMapType;
+	//Вектор дескрипторов открытых библиотек и соответствующих им функций очистки 
+	LibrariesMapType m_mapLibraries;
+
+};
+
+
+#endif /*PLUGINLOADSTRATEGY_H_*/
