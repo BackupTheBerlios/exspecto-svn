70753e9be1f07461bcac634544a22670fca8bb38
parshind <parshind@284ff1dd-de15-0410-891b-f61abf421a23>
2006-11-01 21:21:20 +0000

    Удален лишний MessageBox, немного подправлен NB плагин

--- trunk/Sources/Win32/CommonFiles/CScanner.h
+++ trunk/Sources/Win32/CommonFiles/CScanner.h
@@ -12,7 +12,7 @@ public:
 		
 	virtual void Scan( IN std::string strAddress, OUT std::vector< std::string >& vecResList ) = 0;
 	
-	virtual const char* GetProtocolName();
+	virtual const char* GetProtocolName() = 0;
 
 };
 #endif //CSCANNER_H_
