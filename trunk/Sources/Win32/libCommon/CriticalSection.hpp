e6815d0bcaaabbe21bbf72f9d4f113931a8e48c1
parshind <parshind@284ff1dd-de15-0410-891b-f61abf421a23>
2007-03-16 21:12:10 +0000

    Изменения в структуре агента

--- trunk/Sources/Win32/libCommon/CriticalSection.hpp
+++ trunk/Sources/Win32/libCommon/CriticalSection.hpp
@@ -1,6 +1,8 @@
 #ifndef CRITICALSECTION_HPP_
 #define CRITICALSECTION_HPP_
 
+#include "windows.h"
+
 class CCriticalSection
 {
 public:
@@ -27,4 +29,22 @@ private:
 	CRITICAL_SECTION m_cs;
 };
 
+class CLock
+{
+public:
+
+	CLock( CCriticalSection& cs ):m_cs( cs )
+	{
+		m_cs.Enter();
+	}
+
+	~CLock()
+	{
+		m_cs.Leave();
+	}
+
+private:
+
+	CCriticalSection& m_cs;
+};
 #endif /*CRITICALSECTION_HPP_*/
