0dfe3a8ecde90f40c9109b7406acaab979e51683
parshind <parshind@284ff1dd-de15-0410-891b-f61abf421a23>
2007-04-08 09:41:30 +0000

    Initial import.

---
+++ trunk/Sources/Win32/DBSQLite3/tools.h
@@ -0,0 +1,14 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+#include "CDBProvider.h"
+
+namespace Tools{
+	void Split(string &text, string separators, list<string> &words);
+//	char* GetTimeStr(time_t iTime, char* strRes);
+	void FormatSQLQuery(const string& aText, string& aResult);
+	time_t ConvertFileTimeToUTC(DWORD lFTime, DWORD hFTime); 
+	time_t ConvertFileTimeToUTC(const fileDate& aDate); 
+}
+
+#endif /*TOOLS_H_*/
